242ede193106e84326b871eedd4dc216e94697fc
reffanpanduamirulloh <reffan014@gmail.com>
2019-09-25 19:55:46 +0700

    update tugas (- Parcelable)

--- Intent/app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -3,6 +3,8 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +14,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void handleExplicitIntent(View view) {
+        Intent intent =  new Intent(this, ExplicitIntentActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleExit(View view) {
+        finish();
+    }
+
+    public void handleImplicitIntent(View view) {
+        Intent intent = new Intent(this, ImplicitIntentActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleBundle(View view) {
+        Intent intent = new Intent (this, BundleActivity.class);
+        startActivity(intent);
+    }
 }
---
+++ Intent/app/src/main/java/id/ac/polinema/intent/model/User.java
@@ -0,0 +1,38 @@
+package id.ac.polinema.intent.model;
+
+public class User {
+
+    private String username;
+    private String name;
+    private int age;
+
+    public User(String username, String name, int age) {
+        this.username = username;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
